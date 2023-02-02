33d98a3304271541eca2e747718e4ad018e4933a
IndianGuy2 <zaidshaikh791@gmail.com>
2023-02-02 11:19:04 +0530

    Second Commit
    Buttons Updated

--- lib/sheet_child.dart
+++ lib/sheet_child.dart
@@ -1,38 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StudyMaterial extends StatelessWidget {
   const StudyMaterial({super.key});
 
   @override
+  //final Uri ada = Uri.parse('https://drive.google.com/drive/folders/11LTaCAE4TvB_PwpQDuBgeDr9D8XK4f8u?usp=sharing');
+  //final Uri cn = Uri.parse('https://drive.google.com/drive/folders/1wWbE3stnfrUZkY56p5ts0oF-3wUPP_Z7?usp=share_link');
+  //final Uri wd = Uri.parse('https://drive.google.com/drive/folders/1Uvdn3eaY96zjqcYmryPHQe3IiXLmTsp0?usp=share_link');
+  //final Uri ds = Uri.parse('https://drive.google.com/drive/folders/1yDkpWWGHZOjr3NAMKd2E-Ssn__dnGvtm?usp=share_link');
+  //final Uri pe = Uri.parse('https://drive.google.com/drive/folders/1khKEldo0Bp_HZNMttef63KX_Lnvj9zEz?usp=share_link');
+  //final Uri cpdp = Uri.parse('https://drive.google.com/drive/folders/1gGHFSNkR0ijEHco6miuGyYTZjMebJIiW?usp=share_link');
+
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return SingleChildScrollView(
       child: SizedBox(
-        height: 400,
+        height: mediaQuery.size.height,
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              width: mediaQuery.size.width,
+              padding: const EdgeInsets.all(8.0),
+              child: const Card(
+                color: Colors.amber,
+                child: Center(
+                  child: Text(
+                    'Sem - 5',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/11LTaCAE4TvB_PwpQDuBgeDr9D8XK4f8u?usp=sharing'),
+                ),
+                child: const Text('ADA'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/1wWbE3stnfrUZkY56p5ts0oF-3wUPP_Z7?usp=share_link'),
+                ),
+                child: const Text('CN'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/1Uvdn3eaY96zjqcYmryPHQe3IiXLmTsp0?usp=share_link'),
+                ),
+                child: const Text('WD'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/1yDkpWWGHZOjr3NAMKd2E-Ssn__dnGvtm?usp=share_link'),
+                ),
+                child: const Text('DS'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Button 1'),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/1khKEldo0Bp_HZNMttef63KX_Lnvj9zEz?usp=share_link'),
+                ),
+                child: const Text('PE'),
+              ),
+            ),
+            Container(
+              //height: mediaQuery.size.height * 0.04,
+              width: mediaQuery.size.width * 0.4,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () => launchUrl(
+                  Uri.parse(
+                      'https://drive.google.com/drive/folders/1gGHFSNkR0ijEHco6miuGyYTZjMebJIiW?usp=share_link'),
+                ),
+                child: const Text('CPDP'),
+              ),
             ),
           ],
         ),
